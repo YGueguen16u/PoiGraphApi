2937df7e0d387c39ecebc63b9008c247d07c8226
Sphyren25 <erwanr34@gmail.com>
2025-10-17 10:23:42 +0000

    added docker conf with postgres, jupyter notebook and pySpark

--- src/notebooks/data_acquisition/poi/getCSV.py
+++ src/notebooks/data_acquisition/poi/getCSV.py
@@ -6,9 +6,9 @@ url = "https://static.data.gouv.fr/resources/datatourisme-la-base-nationale-des-
 occitanie_data_file = "dataTourismeOccitanie.csv"
 
 # if local storage of original file is needed, use the following
-response = requests.get(url)
-with open(occitanie_data_file, 'wb') as f:
-    f.write(response.content)
+# response = requests.get(url)
+# with open(occitanie_data_file, 'wb') as f:
+#     f.write(response.content)
 
 #otherwise, use directly pandas by changing 'occitanie_data_file' to 'url'
 df = pd.read_csv(occitanie_data_file, sep = ',', index_col = 0)
