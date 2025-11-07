daa74a6e46ba02ebbef25b15f1d8dbb62113480f
Sphyren25 <erwanr34@gmail.com>
2025-11-07 16:37:20 +0000

    added collection of cities and spark cleaning of datatourisme POI and cities

--- src/notebooks/data_transform/cleanDataTourismePandas.py
+++ src/notebooks/data_transform/cleanDataTourismePandas.py
@@ -2,16 +2,10 @@ import requests
 import pandas as pd
 
 # input parameters
-url = "https://static.data.gouv.fr/resources/datatourisme-la-base-nationale-des-donnees-publiques-dinformation-touristique-en-open-data/20251015-025956/datatourisme-reg-occ.csv"
 occitanie_data_file = "dataTourismeOccitanie.csv"
+filepath = "data/raw/" + occitanie_data_file
 
-# if local storage of original file is needed, use the following
-# response = requests.get(url)
-# with open(occitanie_data_file, 'wb') as f:
-#     f.write(response.content)
-
-#otherwise, use directly pandas by changing 'occitanie_data_file' to 'url'
-df = pd.read_csv(occitanie_data_file, sep = ',', index_col = 0)
+df = pd.read_csv(filepath, sep = ',', index_col = 0)
 
 # basic data exploration
 print("first 20 rows of data:", df.head(20))
---
+++ src/notebooks/data_transform/cleanDataTourismeSpark.py
@@ -0,0 +1,111 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.conf import SparkConf
+from pyspark import SparkContext
+
+conf = SparkConf()
+conf.set("spark.log.level", "error") # To display only errors
+conf.set("spark.ui.showConsoleProgress", "false") # To display only Spark jobs progression in Python
+
+### definition of SparkContext
+sc = SparkContext.getOrCreate(conf=conf) 
+
+spark = SparkSession\
+        .builder\
+        .appName("projet_POI")\
+        .master("local[*]")\
+        .getOrCreate()
+
+### open existing datafile ensuring accurate data types
+occitanie_data_file = "dataTourismeOccitanie.csv"
+
+data_tourisme_schema = StructType([StructField("Nom_du_POI", StringType()),\
+                    StructField("Categories_de_POI", StringType()),\
+                    StructField("Latitude", FloatType()),\
+                    StructField("Longitude", FloatType()),\
+                    StructField("Adresse_postale", StringType()),\
+                    StructField("Code_postal_et_commune", StringType()),\
+                    StructField("Periodes_regroupees", StringType()),\
+                    StructField("Covid19_mesures_specifiques", StringType()),\
+                    StructField("Createur_de_la_donnee", StringType()),\
+                    StructField("SIT_diffuseur", StringType()),\
+                    StructField("Date_de_mise_a_jour", DateType()),\
+                    StructField("Contacts_du_POI", StringType()),\
+                    StructField("Classements_du_POI", StringType()),\
+                    StructField("Description", StringType()),\
+                    StructField("URI_ID_du_POI", StringType())
+                    ])
+
+df = spark.read.schema(data_tourisme_schema)\
+               .option("delimiter", ",")\
+               .option("header", True)\
+               .option("multiline", True)\
+               .option("escape", "\"")\
+               .csv(occitanie_data_file)
+
+
+
+### columns selection
+df_select = df.drop(*[
+    'Periodes_regroupees',
+    'Covid19_mesures_specifiques',
+    'Createur_de_la_donnee',
+    'SIT_diffuseur',
+    'Classements_du_POI',
+    'URI_ID_du_POI'])
+
+### columns split
+
+# first, split 'Code_postal_et_commune' in two atomic parts, clean 'Code_postal' and cast it to 'int'
+df_split = df_select.withColumn('Code_postal', split(df_select['Code_postal_et_commune'], '#')[0])\
+    .withColumn('Commune', split(df_select['Code_postal_et_commune'], '#')[1])
+df_split = df_split.drop('Code_postal_et_commune')
+df_split = df_split.withColumn('Code_postal', regexp_replace(df_split['Code_postal'], ' ',''))
+df_split = df_split.withColumn('Code_postal', df_split['Code_postal'])
+
+# maenwhile, select only postal code corresponding to montpellier
+df_split_mtp = df_split.where((col('Code_postal') >= 34000) & (col('Code_postal') < 34100))
+
+# second, split 'Categories_de_POI' into 2 (for montpellier) or 3 categories (for whole occitanie) 
+# Montpellier case
+df_split_2 = df_split_mtp.withColumn('Cat_POI_first_init', split(df_split_mtp['Categories_de_POI'], '#')[1])\
+    .withColumn('Cat_POI_second_init', split(df_split_mtp['Categories_de_POI'], '#')[2])
+
+df_split_3 = df_split_2.withColumn('Categories_de_POI_niveau1', split(df_split_2['Cat_POI_first_init'], '\|')[0])\
+    .withColumn('Categories_de_POI_niveau2', split(df_split_2['Cat_POI_second_init'], '\|')[0])
+df_split_3.select(['Categories_de_POI_niveau1', 'Categories_de_POI_niveau2']).show(5)
+df_split_3 = df_split_3.drop(*['Categories_de_POI', 'Cat_POI_first_init', 'Cat_POI_second_init'])
+
+
+# Occitanie case, uncomment when ready
+# df_split_2 = df_split.withColumn('Cat_POI_first_init', split(df_split['Categories_de_POI'], '#')[1])\
+#     .withColumn('Cat_POI_second_init', split(df_split['Categories_de_POI'], '#')[2])\
+#     .withColumn('Cat_POI_third_init', split(df_split['Categories_de_POI'], '#')[3])
+
+# df_split_3 = df_split_2.withColumn('Categories_de_POI_niveau1', split(df_split_2['Cat_POI_first_init'], '\|')[0])\
+#     .withColumn('Categories_de_POI_niveau2', split(df_split_2['Cat_POI_second_init'], '\|')[0])\
+#     .withColumn('Categories_de_POI_niveau3', split(df_split_2['Cat_POI_third_init'], '\|')[0])
+# df_split_3.select(['Categories_de_POI_niveau1', 'Categories_de_POI_niveau2', 'Categories_de_POI_niveau3']).show(5)
+# df_split_3 = df_split_3.drop(*['Categories_de_POI', 'Cat_POI_first_init', 'Cat_POI_second_init', 'Cat_POI_third_init'])
+
+### columns cleaning
+df_clean = df_split_3.withColumn('Nom_du_POI', lower(df_split_3['Nom_du_POI']))\
+    .withColumn('Contacts_du_POI', regexp_replace('Contacts_du_POI', '#', ''))\
+    .withColumn('Latitude', round(df_split_3['Latitude'], 6))\
+    .withColumn('Longitude', round(df_split_3['Longitude'], 6))
+
+### deduplication according to POI_name, latitude and longitude 
+
+#for checking purpose only
+# def return_duplicates(df):
+#   return(df.groupBy(df.columns).agg(count("*").alias("duplicates")).filter(col("duplicates") >= 2))
+
+# print("count_duplicates :", return_duplicates(df_clean).sort("duplicates", ascending=False).count())
+# return_duplicates(df_clean).sort("duplicates", ascending=False).show(10)
+# print("df_clean.count :", df_clean.count())
+
+df_unique = df_clean.dropDuplicates(['Nom_du_POI', 'Latitude', 'Longitude'])
+
+
+sc.stop()
